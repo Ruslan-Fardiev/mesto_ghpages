27040ed9f56a5839b90f6b09b72ef867fbda8f06
Ruslan-Fardiev <125750013+Ruslan-Fardiev@users.noreply.github.com>
2023-04-20 20:05:43 +0300

    dev/popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,33 @@
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+let formElement = popupElement.querySelector('.popup__container')
+let nameInput = formElement.querySelector('.popup__name-input');
+let jobInput = formElement.querySelector('.popup__job-input');
+let profileUsername = document.querySelector('.profile__username');
+let profileAboutMe = document.querySelector('.profile__paragrath');
 
-const togglePopupVisibility = function() {
-    popupElement.classList.toggle('popup_opened');
+const addPopup = function() {
+    nameInput.value = profileUsername.textContent;
+    jobInput.value = profileAboutMe.textContent;
+    popupElement.classList.add('popup_opened');
+    return undefined;
 }
 
-popupOpenButtonElement.addEventListener('click', togglePopupVisibility);
-popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
+const removePopup = function() {
+    popupElement.classList.remove('popup_opened');
+    return undefined;
+}
+
+const handleFormSubmit = function(evt) {
+    evt.preventDefault();
+    profileUsername.textContent = nameInput.value;
+    profileAboutMe.textContent = jobInput.value;
+    removePopup();
+    return undefined;
+}
+
+popupOpenButtonElement.addEventListener('click', addPopup);
+popupCloseButtonElement.addEventListener('click', removePopup);
+formElement.addEventListener('submit', handleFormSubmit);
 
