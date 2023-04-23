a65b74f4ab8a33ab1c75642fef36120fc7dc002f
Ruslan-Fardiev <125750013+Ruslan-Fardiev@users.noreply.github.com>
2023-04-23 21:33:00 +0300

    bagfix after 1th rewiev

--- scripts/index.js
+++ scripts/index.js
@@ -2,33 +2,32 @@ const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const formElement = popupElement.querySelector('.popup__container')
-let nameInput = formElement.querySelector('.popup__name-input');
-let jobInput = formElement.querySelector('.popup__job-input');
+let nameInput = formElement.querySelector('.popup_input_name');
+let jobInput = formElement.querySelector('.popup_input_job');
 let profileUsername = document.querySelector('.profile__username');
 let profileAboutMe = document.querySelector('.profile__paragrath');
 
+// включение поп-апа
 const addPopup = function() {
     nameInput.value = profileUsername.textContent;
     jobInput.value = profileAboutMe.textContent;
     popupElement.classList.add('popup_opened');
-    return undefined;
 }
 
+// закрытие поп-апа
 const removePopup = function() {
     popupElement.classList.remove('popup_opened');
-    return undefined;
 }
 
-popupOpenButtonElement.addEventListener('click', addPopup);
-popupCloseButtonElement.addEventListener('click', removePopup);
-
+// редактирование данных пользователя
 const handleFormSubmit = function(evt) {
     evt.preventDefault();
     profileUsername.textContent = nameInput.value;
     profileAboutMe.textContent = jobInput.value;
     removePopup();
-    return undefined;
 }
 
+popupOpenButtonElement.addEventListener('click', addPopup);
+popupCloseButtonElement.addEventListener('click', removePopup);
 formElement.addEventListener('submit', handleFormSubmit);
 
